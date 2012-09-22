54160a693af53ab275bd507fa9f29c8ca4771792
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2012-09-23 03:23:55 +1200

    Move closure and rts files to their new locations

--- fay-hello-js.jsexe/hs1min.js
+++
@@ -1,3 +0,0 @@
-var gf=q(1,function(a){j(a,function(a){k(a.a[0])})});q(1,function(a){hf.b(a)});var kf=q(3,function(a,c,b){jf.b(a,c,b)});
-
-//@ sourceMappingURL=hs1min.js.map
--- fay-hello-js.jsexe/hsloader.js
+++
@@ -1,6 +0,0 @@
-// Bundle Count 5
-var $$$Main_compileFromTo=$L([5,4,1], function() { return $$Main_compileFromTo; });
-var $$$Main_compileFromTo1=$L([5,4,1], function() { return $$Main_compileFromTo1; });
-var $$$Main_main=$L([4,3,1], function() { return $$Main_main; });
-var $$$Main_main1=$L([4,3], function() { return $$Main_main1; });
-var $$$Main_main2=$L([4,3,2,1], function() { return $$Main_main2; });
